ea113c8e4f6d352343283023995117522c3e28ef
marcovirinni@gmail.com <marcovirinni@gmail.com>
2024-06-19 15:34:22 -0300

    update

--- universidad/settings.py
+++ universidad/settings.py
@@ -91,6 +91,9 @@ WSGI_APPLICATION = 'universidad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases# Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
